d3093cc477c3ad2636d9ef0598c19a9db521b82e
roblesbraun <roblesbraun@gmail.com>
2022-03-21 19:24:39 -0600

    #4-courses-view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./**.html", "./**.php"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            psipeGreen: '#D9E9E5',
+            psipeGray: '#AFAEB0',
+            psipeBlue: '#4E9CAE',
+            psipeDarkGray: '#5C5C5B',
+        }
+    },
   },
   plugins: [],
 }
